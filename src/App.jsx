8b7a56bbf9fdf9f827a6a4d13cc3ca496c599586
BhukyaLakshman <lakshmanbhukya1709@gmail.com>
2025-04-05 22:10:32 +0530

    chrome for developers intial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from "react";
+import "./App.css";
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="navbar">
+        <img src="/chrome.png" alt="" />
+        <h2>chrome for developers</h2>
+        <ul type="none">
+          <li>Get Inspired</li>
+          <li>Blog</li>
+          <li>Docs</li>
+          <li>New in Chrome</li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className="notice">
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Chrome is back at Google I/O on May 20-21! <a href="">Register!!</a>
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="hero">
+        <h1>
+          <span style={{ color: "#1a73e8", fontWeight: "bold" }}>
+            A Powerful Web.
+          </span>
+          <span style={{ color: "#333", display: "block" }}>Made Easier.</span>
+        </h1>
+        <p>Simplifying the web to help you build, grow and innovate.</p>
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
 }
-
-export default App
